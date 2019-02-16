2377c3806621e3331a1cc7a7c07f820e5cdcc38a
AJ Pahl <ajpahl1008@gmail.com>
2019-02-16 16:43:16 -0500

    Deleting fintech from this repo

--- elasticsearch-fintech-generator/src/main/java/com/pahlsoft/utilities/interfaces/AccountEngine.java
+++
@@ -1,6 +0,0 @@
-package com.pahlsoft.utilities.interfaces;
-
-
-public interface AccountEngine extends Runnable {
-
-}
--- elasticsearch-fintech-generator/src/main/java/com/pahlsoft/utilities/interfaces/TransactionEngine.java
+++
@@ -1,6 +0,0 @@
-package com.pahlsoft.utilities.interfaces;
-
-
-public interface TransactionEngine extends Runnable {
-
-}
